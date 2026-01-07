c67f47a40d0387546e93009978fa8dd24d068359
gmolveau <2090495+gmolveau@users.noreply.github.com>
2026-01-07 22:39:56 +0100

    login (pre)

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
 
 auth_blueprint = Blueprint("auth", __name__)
 
@@ -9,6 +10,20 @@ def register_page():
 
 
 @auth_blueprint.post("/register")
-def register_form():
+def post_register():
     form = request.form.to_dict()
-    print(form)
+    password = form.get("password")
+    secure_password = generate_password_hash(password)
+    print(secure_password)
+
+
+@auth_blueprint.get("/login")
+def login_page():
+    return render_template("login.html")
+
+
+@auth_blueprint.post("/login")
+def post_login():
+    form = request.form.to_dict()
+    password = form.get("password")
+    check_password_hash(secure_password, password)
