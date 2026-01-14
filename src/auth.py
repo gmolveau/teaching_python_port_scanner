6ececaf4143a4ec9014da8ab36fb3213dd4d3718
gmolveau <2090495+gmolveau@users.noreply.github.com>
2026-01-14 19:16:34 +0100

    correction 2026-01-15

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from src.db import USERS
 
 auth_blueprint = Blueprint("auth", __name__)
 
@@ -12,9 +14,15 @@ def register_page():
 @auth_blueprint.post("/register")
 def post_register():
     form = request.form.to_dict()
+    username = form.get("username")
+    if username in USERS:
+        print("user already exists")
+        return redirect(url_for("auth.register_page"))
+
     password = form.get("password")
     secure_password = generate_password_hash(password)
-    print(secure_password)
+    USERS[username] = secure_password
+    return redirect(url_for("auth.login_page"))
 
 
 @auth_blueprint.get("/login")
@@ -25,5 +33,13 @@ def login_page():
 @auth_blueprint.post("/login")
 def post_login():
     form = request.form.to_dict()
+    username = form.get("username")
+    if username not in USERS:
+        return redirect(url_for("auth.login_page"))
+
+    secure_password = USERS[username]
     password = form.get("password")
-    check_password_hash(secure_password, password)
+    if check_password_hash(secure_password, password):
+        return redirect(url_for("forms.home"))
+    else:
+        return redirect(url_for("auth.login_page"))
